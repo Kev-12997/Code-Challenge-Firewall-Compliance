ac631d569002b7d608c361730f03df97cc409508
krodriguez <kevin.rodrivera@gmail.com>
2024-08-22 19:08:13 -0400

    Code refactoring

--- compliant_firewall_rules.py
+++ compliant_firewall_rules.py
@@ -11,13 +11,13 @@ Compliance requirement: Firewall rules shall NOT allow ingress(Inbound) traffic 
 91.172.88.105
 """
 PORTS = [22,80,443, -1] #Ports of interest for ingress rules, -1 means every port.
-IPADDRESSES = set([ipaddress.IPv4Address("236.216.246.119"), #List of Ip's that should NOT allow ingress trafic on ports from the PORTS array.
+IP_ADDRESSES = set([ipaddress.IPv4Address("236.216.246.119"), #List of Ip's that should NOT allow ingress trafic on ports from the PORTS array.
             ipaddress.IPv4Address("109.3.194.189"), #Sets are generally faster.
             ipaddress.IPv4Address("36.229.68.87"),  
             ipaddress.IPv4Address("21.90.154.237"),
             ipaddress.IPv4Address("91.172.88.105")])
 
-FIREWALLRULESENDPOINT = "https://g326av89lk.execute-api.us-east-1.amazonaws.com/prod/rules" #Endpoint with firewall rules
+FIREWALL_RULES_ENDPOINT = "https://g326av89lk.execute-api.us-east-1.amazonaws.com/prod/rules" #Endpoint with firewall rules
              
 async def get_firewall_rules(pagination=None):
     """
@@ -27,35 +27,41 @@ async def get_firewall_rules(pagination=None):
         pagination: A dictionary containing pagination information for fetching the next set of results. Defaults to None.
     """
     async with aiohttp.ClientSession() as session:
+        results = [] #Array that will contain the json array object of the final output
+        current_pagination = pagination #Tracks current page
+        
         try:
-            results = []
+            while True: #Will remain true as long as the current page has a LastEvaluatedKey value
             
-            if pagination is None:
-                async with session.get(FIREWALLRULESENDPOINT) as response: #Get request to url
-                    response.raise_for_status()  # Raise error for any HTTP related issues
-                    data = await response.json()  # Await the JSON response
-                    results += check_if_direction_is_ingress(data['Items'])
-            else:
-                last_evaluated_key = json.dumps(pagination) #The pagination portion of the url needs to be in json format
-                async with session.get(f"{FIREWALLRULESENDPOINT}?ExclusiveStartKey={last_evaluated_key}") as response: #Append the pagination to url
-                    response.raise_for_status()  
-                    data = await response.json()
-                    results += check_if_direction_is_ingress(data['Items'])
+                if current_pagination is None: 
+                    url = FIREWALL_RULES_ENDPOINT
+                else:
+                    last_evaluated_key = json.dumps(current_pagination)
+                    url = f"{FIREWALL_RULES_ENDPOINT}?ExclusiveStartKey={last_evaluated_key}"
 
-            if 'LastEvaluatedKey' in data: #If the value LastEvaluatedKey exists in the JSON response. We are still not in the last page
-                lastEvaluatedKey = data['LastEvaluatedKey']
-                results += await get_firewall_rules(lastEvaluatedKey) #Recursively fetch the next page 
+                async with session.get(url) as response:
+                    response.raise_for_status() # Raise error for any HTTP related issues
+                    data = await response.json() # Await the JSON response
+
+                results.extend(check_if_direction_is_ingress(data['Items']))
+
+                if 'LastEvaluatedKey' not in data:
+                    break #Break out of while loop when reaching the last page
+
+                current_pagination = data['LastEvaluatedKey']
                 
-            return results
         except aiohttp.ClientError as e: #Any HTTP errors
             print(f"HTTP error: {e}")
+            raise
         except Exception as e: #Any other type of errors
             print(f"Unexpected error: {e}")
+                
+        return results
             
 def check_if_direction_is_ingress(items):
     """
     Checks if the rules from the current page are related to ingress. Since the compliance requirement 
-    is only for the ingress direction, any other direction will default to 'NON_COMPLIANT' an no other check will be done.
+    is only for the ingress direction, any other direction will default to 'COMPLIANT' an no other check will be done.
     
     Args:
         items: A dictionary containing the rules from the current page
@@ -65,8 +71,7 @@ def check_if_direction_is_ingress(items):
         if rule['Direction'] == 'Ingress': #If the direction is Ingress, this rule is of interest and we will proceed to look at the ports
             results.append(check_ports(rule))
         else:
-            results.append({"RuleId": rule['RuleId'],  # None Ingress direction, mark as NON_COMPLIANT
-                            "Compliance": "NON_COMPLIANT"})
+            results.append(compliant_rule(rule))  # None Ingress direction, mark as COMPLIANT
     return results
             
 def check_ports(rule):
@@ -81,9 +86,8 @@ def check_ports(rule):
         if rule['FromPort'] <= port <= rule['ToPort']:
             return check_ip_addresses(rule)
     
-    # If none of the sensitive ports are in the range, NON_COMPLIANT
-    return {"RuleId": rule['RuleId'],  
-            "Compliance": "NON_COMPLIANT"}
+    # If none of the sensitive ports are in the range, COMPLIANT
+    return compliant_rule(rule)
         
 def check_ip_addresses(rule):
     """
@@ -100,14 +104,23 @@ def check_ip_addresses(rule):
             print(f"Skipping invalid network '{ip_network}': {e}")
     
     for network in networks: #For every network in the CIDR range
-        for ip in IPADDRESSES: #For every IP in the IPADDRESSES set
+        for ip in IP_ADDRESSES: #For every IP in the IPADDRESSES set
             if ip in network: #Does the current IPADDRESSES exist in the subnet of the current network iteration?
                 if rule['Action'] == 'Deny':  # If it exists and the action is 'Deny', the rule is COMPLIANT
+                    return compliant_rule(rule)
+                else: # Only rules that are non compliant are rules that DO allow trafic over the sensitive ports on the specified IPs
                     return {"RuleId": rule['RuleId'],
-                            "Compliance": "COMPLIANT"}
-    # If no IP from IPADDRESSES is found in the network or the action is not 'Deny'
+                            "Compliance": "NON_COMPLIANT"}
+                    
+    # If no IP from IPADDRESSES is found in the network
+    return compliant_rule(rule)
+
+"""
+Returns the COMPLIANT json
+"""
+def compliant_rule(rule):
     return {"RuleId": rule['RuleId'],
-            "Compliance": "NON_COMPLIANT"}
+            "Compliance": "COMPLIANT"}
             
 async def main():
     results = await get_firewall_rules()
--- test_compliant_firewall_rules.py
+++ test_compliant_firewall_rules.py
@@ -1,10 +1,28 @@
 import pytest
+import aiohttp
 from compliant_firewall_rules import check_if_direction_is_ingress, check_ports, check_ip_addresses, get_firewall_rules, PORTS
 
-def test_check_if_valid_direction_is_compliant():
+def test_check_if_direction_is_not_ingress():
     rule = [
         {
             "RuleId": "102",
+            "Direction": "Egress",
+            "FromPort": 22,
+            "ToPort": 22,
+            "IpRanges": ["236.216.246.119"],
+            "Action": "Allow"
+        }
+    ]
+    
+    expected_output = [
+        {"RuleId": "102", "Compliance": "COMPLIANT"}
+    ]
+    assert check_if_direction_is_ingress(rule) == expected_output
+    
+def test_check_if_direction_is_ingress():
+    rule = [
+        {
+            "RuleId": "100",
             "Direction": "Ingress",
             "FromPort": 22,
             "ToPort": 22,
@@ -14,30 +32,12 @@ def test_check_if_valid_direction_is_compliant():
     ]
     
     expected_output = [
-        {"RuleId": "102", "Compliance": "COMPLIANT"}
+        {"RuleId": "100", "Compliance": "COMPLIANT"}
     ]
     
     assert check_if_direction_is_ingress(rule) == expected_output
     
-def test_check_if_invalid_direction_is_not_compliant():
-    rule = [
-        {
-            "RuleId": "100",
-            "Direction": "Egress",
-            "FromPort": 22,
-            "ToPort": 22,
-            "IpRanges": ["236.216.246.119"],
-            "Action": "Deny"
-        }
-    ]
-    
-    expected_output = [
-        {"RuleId": "100", "Compliance": "NON_COMPLIANT"}
-    ]
-    
-    assert check_if_direction_is_ingress(rule) == expected_output
-    
-def test_check_if_valid_ports_are_compliant():
+def test_check_if_relevant_ports_are_compliant():
   for port in PORTS:
     rules = {
             "RuleId": "200",
@@ -50,19 +50,19 @@ def test_check_if_valid_ports_are_compliant():
     expected_output = {"RuleId": "200", "Compliance": "COMPLIANT"}
     assert check_ports(rules) == expected_output
     
-def test_check_if_invalid_ports_are_not_compliant():
+def test_check_if_irrelevant_ports_are_compliant():
   rule = {
           "RuleId": "200",
           "Direction": "Ingress",
           "FromPort": 10,
           "ToPort": 20,
           "IpRanges": ["236.216.246.119"],
-          "Action": "Deny" 
+          "Action": "Allow" 
           }
-  expected_output = {"RuleId": "200", "Compliance": "NON_COMPLIANT"}
+  expected_output = {"RuleId": "200", "Compliance": "COMPLIANT"}
   assert check_ports(rule) == expected_output
   
-def test_check_if_ip_in_range_is_valid():
+def test_check_if_ip_in_range_is_compliant():
   rule = {
           "RuleId": "200",
           "Direction": "Ingress",
@@ -77,17 +77,24 @@ def test_check_if_ip_in_range_is_valid():
   expected_output = {"RuleId": "200", "Compliance": "COMPLIANT"}
   assert check_ip_addresses(rule) == expected_output
   
-def test_check_if_ip_in_range_is_not_valid():
+def test_check_if_ip_in_range_is_not_compliant():
   rule = {
           "RuleId": "200",
           "Direction": "Ingress",
           "FromPort": 22,
           "ToPort": 22,
-          "IpRanges": ["78.76.115.63/14",
+          "IpRanges": ["236.216.246.119/32",
+                       "78.76.115.63/14",
                       "114.131.46.115/30",
                       "221.98.94.123/22"],
-          "Action": "Deny" 
+          "Action": "Allow" 
           }
   expected_output = {"RuleId": "200", "Compliance": "NON_COMPLIANT"}
   assert check_ip_addresses(rule) == expected_output
   
+@pytest.mark.asyncio
+async def test_http_error_code(monkeypatch):
+    monkeypatch.setattr('compliant_firewall_rules.FIREWALL_RULES_ENDPOINT', 'https://httpbin.org/status/500')
+    
+    with pytest.raises(aiohttp.ClientError):
+        await get_firewall_rules()
